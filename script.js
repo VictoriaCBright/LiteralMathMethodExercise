fc6a49740f6da2cfb43e76fa007341e7860d867c
Victoria Bright <victoriacbright92@gmail.com>
2022-09-29 10:37:11 -0500

    Updated

--- script.js
+++ script.js
@@ -4,27 +4,36 @@ let warmHugs = "Hi, I'm Olaf and I like warm hugs."; console.log(warmHugs)
 console.log(warmHugs.toUpperCase()); 
 //1c
 console.log(warmHugs.replace("like", "love"));
+// warmHugs = warmHugs.replace("like", "love"); console.log(warmHugs);
 //2a
-var beenImpaled = "Oh, look at that. I've been impaled."; console.log(beenImpaled);
+let beenImpaled = "Oh, look at that. I've been impaled."; console.log(beenImpaled);
 //2b
 console.log(beenImpaled.slice(18, 36));
+//console.log(beenImpaled.slice(-18));
 //3a
-var dotDotDot = "...";
+let dotDotDot = "...";
 //3b
 let skullBones= `I don\'t have a skull${dotDotDot}or bones.`; console.log(skullBones);
 //4
 console.log(Math.PI);
 //5
-var randomNumber = Math.random();
+/* let randomNumber = Math.random();
 randomNumber = randomNumber *3;
-console.log(randomNumber);
+randomNumber = Math.floor(randomNumber);
+randomNumber ++;
+console.log(randomNumber); */
+
+//let randomNumber = Math.floor(Math.random()*3 +1); console.log(randomNumber);
+
+let randomNumber = Math.ceil(Math.random () *3); console.log(randomNumber);
 
 //BONUS
 //6
 //repeatStringNumTimes(" Let It Go!", 2);
-console.log(" Let It Go!".repeat(2));
+console.log(" Let It Go!".repeat(2).toUpperCase());
+
 //7a
-var reindeers = "Reindeers are better than people.";
+let reindeers = "Reindeers are better than people.";
 //7b
 console.log(reindeers.replaceAll(" ", "_"));
 //8
